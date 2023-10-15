65feb24577d38df13858ee8596d38941ada62a39
mamadkhezri <138501134+mamadkhezri@users.noreply.github.com>
2023-10-15 19:51:00 +0330

    Update urls.py

--- vitica/urls.py
+++ vitica/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.conf import settings
 from django.conf.urls.static import static
 from django.urls import path, include
+from django.views.static import serve
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -25,6 +26,13 @@ urlpatterns = [
     path('accounts/', include('accounts.urls')),
     path('capsules/', include('capsules.urls'))
 ]
+
+if settings.DEBUG:
+    urlpatterns += [
+        url(r'^media/(?P<path>.*)$', serve, {
+            'document_root': settings.MEDIA_ROOT,
+        }),
+    ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
